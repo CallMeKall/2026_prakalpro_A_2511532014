b933efa153639559160467bca744d6914ac1c44e
Haykal <haykalmdahnil@gmail.com>
2026-09-01 10:18:01 +0700

    praktikum1 alpro selasa

---
+++ pekan1/program1.py
@@ -0,0 +1,13 @@
+# Program pertama
+"""
+Komentar
+dua baris atau lebih
+pertemuan 1 praktikum alpr
+~~~
+"""
+print("Hello World")
+print()
+print("Program ini menghasilkan")
+print("Empat baris output")
+print("/ \\ // \\\\ /// \\\\\\")
+print("Program ini" + " dibuat oleh a\n" + "Haykal Muhammad Dahnil 2511532014")
