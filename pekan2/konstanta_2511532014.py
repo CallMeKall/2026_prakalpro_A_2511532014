f0eec3ac1fd8b1f82ee5a86f7a384a57d905c072
Haykal <haykalmdahnil@gmail.com>
2026-09-08 09:48:35 +0700

    Prak Alpro Pekan2 selasa

--- pekan2/konstanta_2511532014.py
+++ pekan2/konstanta_2511532014.py
@@ -1,6 +1,6 @@
 from typing import Final
-PI: Final = 3.14
-print("pi: %f" % (PI))
+PI_2014: Final = 3.14
+print("pi: %f" % (PI_2014))
 jari_2014 = float(input("Masukan jari-jari lingkaran: "))
-luas_2014 = PI * jari_2014 * jari_2014
+luas_2014 = PI_2014 * jari_2014 * jari_2014
 print("Luas lingkaran dengan jari-jari %.2f adalah %.2f" % (jari_2014, luas_2014))
